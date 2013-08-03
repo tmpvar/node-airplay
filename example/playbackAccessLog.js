d4e5b19b52e05308940e1c56e738b8189f146cb0
Elijah Insua <tmpvar@gmail.com>
2013-08-02 22:35:44 -0700

    add binary plist parser

--- example/playbackAccessLog.js
+++ example/playbackAccessLog.js
@@ -2,13 +2,10 @@ var airplay = require('../lib/airplay');
 var b = airplay.createBrowser();
 
 b.on('deviceOnline', function(deviceInfo) {
-
   var device = new airplay.Device(deviceInfo.id, deviceInfo.info_, function() {
     device.play('http://download.ted.com/talks/JulieTaymor_2011.mp4?apikey=TEDDOWNLOAD', 0, console.log);
     setTimeout(function() {
-
       device.playbackAccessLog(console.log);
-
     }, 2000);
   });
 });
